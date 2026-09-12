8a61eba1935d24d7bef07ca86f83e59ac9931849
Dmitriy Afanasyev <dmtr.afanasyev@gmail.com>
2026-09-12 20:51:58 +0400

    feat: dockerize app, add logging and auto-migrations

--- src/infrastructure/database.py
+++ src/infrastructure/database.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite3"
+from src.core.settings import settings
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
